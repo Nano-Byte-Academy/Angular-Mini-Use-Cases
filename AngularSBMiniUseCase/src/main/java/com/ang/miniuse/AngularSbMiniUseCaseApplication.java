1643c3c0c2f23772d2316443936878b411da3f20
prosenjit005 <167856468+Nano-Byte-Academy@users.noreply.github.com>
2024-05-18 19:40:55 +0530

    initial commit

---
+++ AngularSBMiniUseCase/src/main/java/com/ang/miniuse/AngularSbMiniUseCaseApplication.java
@@ -0,0 +1,13 @@
+package com.ang.miniuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSbMiniUseCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSbMiniUseCaseApplication.class, args);
+	}
+
+}
